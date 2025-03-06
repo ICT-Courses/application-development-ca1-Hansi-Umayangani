a0199ecca42f5c661a17284e94b4c6a992d77b60
Hansi-Umayangani <ar112502@fhss.sjp.ac.lk>
2025-03-07 03:34:59 +0530

    Added main login UI and created event callback functions

---
+++ AquaPOS/User_Login.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AquaPOS
+{
+    /// <summary>
+    /// Interaction logic for MainWindo.xaml
+    /// </summary>
+    public partial class User_Login : Window
+    {
+        public User_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CreateAccountButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ShowPasswordCheckbox_Checked(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ShowPasswordCheckbox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+    }
+}

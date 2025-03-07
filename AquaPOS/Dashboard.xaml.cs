f0baf4da806f6536005be8ed5f2776e488e0c1b5
Hansi-Umayangani <ar112502@fhss.sjp.ac.lk>
2025-03-07 11:52:13 +0530

    Initialize Dashboard UI Create navigation panel of Dashboard UI Create event handler functions to handle the Dashboard button clicks

---
+++ AquaPOS/Dashboard.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AquaPOS
+{
+    /// <summary>
+    /// Interaction logic for Dashboard.xaml
+    /// </summary>
+    public partial class Dashboard : Window
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+
+        private void DashboardButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void InventoryManagementButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SalesReportsButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}

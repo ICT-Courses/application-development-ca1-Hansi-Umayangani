2b59536e084556c59d1f18f024bfa0027edac2e3
Hansi-Umayangani <ar112502@fhss.sjp.ac.lk>
2025-05-03 21:52:23 +0530

    Create Sales_Reports UI - Design Sales Reports window with sidebar navigation and custom styles

---
+++ AquaPOS/Sales_Reports.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AquaPOS
+{
+    /// <summary>
+    /// Interaction logic for Sales_Reports.xaml
+    /// </summary>
+    public partial class Sales_Reports : Window
+    {
+        public Sales_Reports()
+        {
+            InitializeComponent();
+        }
+    }
+}

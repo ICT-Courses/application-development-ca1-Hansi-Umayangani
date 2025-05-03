773f3f6d34ec0c096401bf3d4e9eae5d34728176
Hansi-Umayangani <ar112502@fhss.sjp.ac.lk>
2025-05-03 20:11:52 +0530

    Create Inventory Management UI

---
+++ AquaPOS/Inventory_Management.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AquaPOS
+{
+    /// <summary>
+    /// Interaction logic for Inventory_Management.xaml
+    /// </summary>
+    public partial class Inventory_Management : Window
+    {
+        public Inventory_Management()
+        {
+            InitializeComponent();
+        }
+    }
+}

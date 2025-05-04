f6cec64709f7442bdade1b86e167c997a4eef917
Hansi-Umayangani <ar112502@fhss.sjp.ac.lk>
2025-05-04 22:07:07 +0530

    Implement Navigation process of Sales Reports window

--- AquaPOS/Sales_Reports.xaml.cs
+++ AquaPOS/Sales_Reports.xaml.cs
@@ -26,22 +26,30 @@ namespace AquaPOS
 
         private void DashboardButton_Click(object sender, RoutedEventArgs e)
         {
-
+            Dashboard dashboard = new Dashboard();
+            dashboard.Show();
+            this.Close();
         }
 
         private void InventoryManagementButton_Click(object sender, RoutedEventArgs e)
         {
-
+            Inventory_Management inventory_Management = new Inventory_Management();
+            inventory_Management.Show();
+            this.Close();
         }
 
         private void SalesReportsButton_Click(object sender, RoutedEventArgs e)
         {
-
+            Sales_Reports sales_Reports = new Sales_Reports();
+            sales_Reports.Show();
+            this.Close();
         }
 
         private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
-
+            User_Login user_Login = new User_Login();
+            user_Login.Show(); // Show the login window
+            this.Hide(); // Close the current window
         }
     }
 }
